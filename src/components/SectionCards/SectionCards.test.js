93f3724d2f26a4569560a9d6cbfc381986c2d3e8
Igor Soares <igorgomes.13ig@gmail.com>
2022-08-18 13:25:48 -0300

    test: add cenaries todo of tests

---
+++ src/components/SectionCards/SectionCards.test.js
@@ -0,0 +1,6 @@
+describe("<SectionCards />", () => {
+  it.todo("Should render component correctly");
+  it.todo("Should render component loading");
+  it.todo("Should render component error");
+  it.todo("Should render component correctly with informations inside cards");
+});
